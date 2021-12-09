41b9f0a9fea9c87f32626c1c32e4c1941a0dd50e
Vishnu Kaushik <vishnu.kaushik@mongodb.com>
2021-12-09 23:42:07 +0000

    Implemented the acceptor, but have not tested anything... lol

--- paxos_simple/paxos_node.py
+++ paxos_simple/paxos_node.py
@@ -19,18 +19,51 @@ class PaxosSimpleMessage(BaseMessage):
 
 class PaxosSimpleAcceptor(BaseNode):
     """
+    An acceptor from the paper.
     """
-    pass
+
+    def __init__(self, addr, all_addresses):
+        super().__init__(self, addr, all_addresses)
+        self.promised = -1
+        self.accepted_value = None
+        self.accepted_num = None
+
+    def __str__(self):
+        return f'''{{"prom": {self.promised}, "acc_v": {self.accepted_value},
+                "acc_num": {self.accepted_num}'''
+
+    def __repr__(self):
+        return self.__str__()
+    
+    def receive_message(self, m, from_addr):
+        if m.message_type == PHASE1_PREPARE:
+            if self.promised > m.body["proposal_num"]:
+                # Ignore the prepare if we have already promised not to accept.
+                return
+
+            body = {}
+            if self.accepted_num is not None:
+                body["accepted_num"] = self.accepted_num
+                body["accepted_value"] = self.accepted_value
+
+            ack_message = PaxosSimpleMessage(PREPARE_ACK, body)
+            self.send_message(ack_message, from_addr)
+
+        elif m.message_type == PHASE2_ACCEPT:
+            if self.accepted_num is None or m.body["proposal_num"] > self.accepted_num:
+                self.accepted_num = m.body["proposal_num"]
+                self.accepted_value = m.body["value"]
+                self.send_message(PaxosSimpleMessage(ACCEPT_ACK, {}), from_addr)
 
 class PaxosSimpleProposer(BaseNode):
     """
     A proposer from the paper.
     """
 
-    def __init__(self, addr, all_addresses):
+    def __init__(self, addr, all_addresses, proposal_num):
         super().__init__(addr, all_addresses)
         self.value = None
-        self.proposal_num = addr
+        self.proposal_num = proposal_num
         self.acceptor_addresses = ['acceptor' in all_addresses]
         self.p1_acks = {}
         self.p2_acks = {}
@@ -38,14 +71,24 @@ class PaxosSimpleProposer(BaseNode):
         self.highest_proposal_num_seen = -1
         self.chosen = False
 
+        self.phase_2 = False
+
+    def __str__(self):
+        return f'''{{"value": {self.value}, "pn": {self.proposal_num},
+                "p1_acks": {dict_to_str(self.p1_acks)}, "p2_acks": {dict_to_str(self.p2_acks)},
+                "highest_prop": {self.highest_proposal_num_seen}, "chosen": {self.chosen}}}'''
+
+    def __repr__(self):
+        return self.__str__()
+
     def receive_message(self, m, from_addr):
         if m.message_type == CLIENT_VALUE:
             self.start_paxos(m)
-        elif m.message_type == PHASE1_ACK:
+        elif m.message_type == PREPARE_ACK:
             self.receive_prepare_ack(m, from_addr)
-        elif m.message_type == PHASE2_ACK:
+        elif m.message_type == ACCEPT_ACK:
             self.receive_accept_ack(m, from_addr)
-    
+
     def start_paxos(self, m):
         """
         Register the value that this proposer is trying to propose, and then start off Paxos by
@@ -63,13 +106,17 @@ class PaxosSimpleProposer(BaseNode):
         The response to a PREPARE is either that no proposal has been accepted so far, or that a
         proposal has been accepted, with that proposal's proposal number.
         """
+        if self.phase_2:
+            return
+
         self.p1_acks[from_addr] = 1
-        if "accepted_proposal" in m.body:
-            if m.body["proposal_num"] > self.highest_proposal_num_seen:
-                self.highest_proposal_num_seen = m.body["proposal_num"]
-                self.value = m.body["accepted_proposal"]
-        
+        if "accepted_num" in m.body:
+            if m.body["accepted_num"] > self.highest_proposal_num_seen:
+                self.highest_proposal_num_seen = m.body["accepted_num"]
+                self.value = m.body["accepted_value"]
+
         if len(self.p1_acks) > (len(self.acceptor_addresses) // 2 + 1):
+            self.phase_2 = True
             # Send 'accept' messages.
             for acc in self.acceptor_addresses:
                 accept_message = PaxosSimpleMessage(
